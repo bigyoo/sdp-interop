1394896bd8fa9bc4080fe0a4cb55cec163b7ede6
joe <ban.jozsef@sonrisa.hu>
2019-10-02 14:58:19 +0200

    reproduction of safari issue

--- test/mytest.js
+++ test/mytest.js
@@ -83,34 +83,20 @@ QUnit.test('audioInactiveUnifiedPlan2PlanB', function (assert) {
 
 QUnit.test('01-planb-offer-from-jvb-conversion-to-unified-plan', function(assert) {
 
+	const interop = new Interop();
+	const rtxModifier = new RtxModifier.default();
+
 	// ------------------------------------------------------------------------------------------
 	// ~~~ Offer from JVB
 	// ------------------------------------------------------------------------------------------
 
 	// read SDP offer that was coming from JVB
-	const planb = fs.readFileSync(BASE_PATH_OF_SAFARI_SDP_FOLDER + '01-offer-from-jvb.txt', 'utf8');
+	const offerPlanb = fs.readFileSync(BASE_PATH_OF_SAFARI_SDP_FOLDER + '01-offer-from-jvb.txt', 'utf8');
 	var offerRTCSessionDesc = new RTCSessionDescription({
 		type: 'offer',
-		sdp: planb
+		sdp: offerPlanb
 	});
-
-	// apply munge transformation
-	offerRTCSessionDesc = simulcast.mungeRemoteDescription(offerRTCSessionDesc);
-	assertAfterMunged(assert, offerRTCSessionDesc.sdp);
-
-	// apply stripRtx transformation
-	const rtxModifier = new RtxModifier.default();
-	offerRTCSessionDesc.sdp = rtxModifier.stripRtx(offerRTCSessionDesc.sdp);
-	assertAfterRtxModification(assert, offerRTCSessionDesc.sdp);
-
-	// apply unified plan transformation
-	const interop = new Interop();
-	offerRTCSessionDesc = interop.toUnifiedPlan(offerRTCSessionDesc);
-	assertAfterUnifiedPlanTransformation(assert, offerRTCSessionDesc.sdp);
-
-	// apply simulcast transformation
-	offerRTCSessionDesc = _insertUnifiedPlanSimulcastReceive(offerRTCSessionDesc);
-	assertAfterSimulcastTransformation(assert, offerRTCSessionDesc.sdp);
+	setOfferAsRemoteDesciption(assert, interop, rtxModifier, offerRTCSessionDesc);
 
 	// ------------------------------------------------------------------------------------------
 	// ~~~ Caching unified plan offer
@@ -138,18 +124,95 @@ QUnit.test('01-planb-offer-from-jvb-conversion-to-unified-plan', function(assert
 	answerRTCSEssionDesc = _injectSsrcGroupForUnifiedSimulcast(answerRTCSEssionDesc);
 	// apply munge transformation
 	answerRTCSEssionDesc = simulcast.mungeLocalDescription(answerRTCSEssionDesc);
-	assertAnswerAfterEveryTransformationApplied(assert, answerRTCSEssionDesc.sdp);
-
+	assertAnswerAfterEveryTransformationApplied(assert, answerRTCSEssionDesc.sdp, '09-answer-after-every-transformation-applied.txt');
+	
 	// --------------------------------------------------------------------------------------------
 	// ~~~ SetLocalDescription
 	// --------------------------------------------------------------------------------------------
-
+	
 	answerRTCSEssionDesc = _adjustLocalMediaDirection(answerRTCSEssionDesc);
 	answerRTCSEssionDesc = _ensureSimulcastGroupIsLast(answerRTCSEssionDesc);
 	answerRTCSEssionDesc = interop.toUnifiedPlan(answerRTCSEssionDesc);
 	assertAnswerBeforeSettingLocalDesc(assert, answerRTCSEssionDesc.sdp);
+	
+	// --------------------------------------------------------------------------------------------
+	// ~~~ Renegotiation - setting new offer as remote desc
+	// --------------------------------------------------------------------------------------------
+	
+	const newOfferPlanb = fs.readFileSync(BASE_PATH_OF_SAFARI_SDP_FOLDER + '12-new-offer-from-jvb.txt', 'utf8');
+	var newOfferRTCSessionDesc = new RTCSessionDescription({
+		type: 'offer',
+		sdp: newOfferPlanb
+	});
+	
+	const newOfferUnified = renegotiation(assert, interop, rtxModifier,  newOfferRTCSessionDesc);
+	
+	// ------------------------------------------------------------------------------------------
+	// ~~~ Renegotiation - creating answer
+	// ------------------------------------------------------------------------------------------
+	
+	const newAnswerSdpGeneratedBySafari = fs.readFileSync(BASE_PATH_OF_SAFARI_SDP_FOLDER + '13-new-answer-generated-by-safari-unified.txt', 'utf8');
+	var newAnswerRTCSEssionDesc = new RTCSessionDescription({
+		type: 'answer',
+		sdp: newAnswerSdpGeneratedBySafari
+	});
+	
+	// apply planB transformation
+	newAnswerRTCSEssionDesc = interop.toPlanB(newAnswerRTCSEssionDesc);
+	// apply simcast transformation
+	newAnswerRTCSEssionDesc = _injectSsrcGroupForUnifiedSimulcast(newAnswerRTCSEssionDesc);
+	// apply munge transformation
+	newAnswerRTCSEssionDesc = simulcast.mungeLocalDescription(newAnswerRTCSEssionDesc);
+	//assertAnswerAfterEveryTransformationApplied(assert, newAnswerRTCSEssionDesc.sdp, '14-new-answer-after-every-transformation-applied.txt');
+	
+	// ------------------------------------------------------------------------------------------
+	// ~~~ Renegotiation - getRemoteDesc
+	// ------------------------------------------------------------------------------------------
+	interop.toPlanB(newOfferUnified);
+	
+	// ------------------------------------------------------------------------------------------
+	// ~~~ Renegotiation - setLocalDesc
+	// ------------------------------------------------------------------------------------------
+
+	interop.toUnifiedPlan(newAnswerRTCSEssionDesc);
 
 });
+
+var setOfferAsRemoteDesciption = function(assert, interop, rtxModifier, offerRTCSessionDesc) {
+	// apply munge transformation
+	offerRTCSessionDesc = simulcast.mungeRemoteDescription(offerRTCSessionDesc);
+		assertAfterMunged(assert, offerRTCSessionDesc.sdp);
+	
+		// apply stripRtx transformation
+		
+		offerRTCSessionDesc.sdp = rtxModifier.stripRtx(offerRTCSessionDesc.sdp);
+		assertAfterRtxModification(assert, offerRTCSessionDesc.sdp);
+	
+		// apply unified plan transformation
+		offerRTCSessionDesc = interop.toUnifiedPlan(offerRTCSessionDesc);
+		assertAfterUnifiedPlanTransformation(assert, offerRTCSessionDesc.sdp);
+	
+		// apply simulcast transformation
+		offerRTCSessionDesc = _insertUnifiedPlanSimulcastReceive(offerRTCSessionDesc);
+		assertAfterSimulcastTransformation(assert, offerRTCSessionDesc.sdp);
+}
+
+var renegotiation = function(assert, interop, rtxModifier, offerRTCSessionDesc) {
+
+	// apply munge transformation
+	offerRTCSessionDesc = simulcast.mungeRemoteDescription(offerRTCSessionDesc);
+
+	// apply stripRtx transformation
+	offerRTCSessionDesc.sdp = rtxModifier.stripRtx(offerRTCSessionDesc.sdp);
+
+	// apply unified plan transformation
+	offerRTCSessionDesc = interop.toUnifiedPlan(offerRTCSessionDesc);
+
+	// apply simulcast transformation
+	offerRTCSessionDesc = _insertUnifiedPlanSimulcastReceive(offerRTCSessionDesc);
+
+	return offerRTCSessionDesc;
+}
 
 var assertAfterMunged = function(assert, actualSdp) {
 	console.log('> Checking after munged.');
@@ -191,12 +254,12 @@ var assertAfterTransformingBackTheOfferToPlanb = function(assert, actualSdp) {
 	assert.equal(expected.trim(), actualSdp.trim());
 }
 
-var assertAnswerAfterEveryTransformationApplied = function(assert, actualSdp) {
-	console.log('> Checking answer after ever Transformation applied');
+var assertAnswerAfterEveryTransformationApplied = function(assert, actualSdp, expectedResultFileName) {
+	console.log('> Checking answer after every Transformation applied');
 	
 	// read expected result
-	const expected = fs.readFileSync(BASE_PATH_OF_SAFARI_SDP_FOLDER + '09-answer-after-every-transformation-applied.txt', 'utf8');
-	assert.equal(expected.trim(), actualSdp.trim());
+	const expected = fs.readFileSync(BASE_PATH_OF_SAFARI_SDP_FOLDER + expectedResultFileName, 'utf8');
+	assert.equal(actualSdp.trim(), expected.trim());
 }
 
 var assertAnswerBeforeSettingLocalDesc = function(assert, actualSdp) {
@@ -356,5 +419,3 @@ var _getDesiredMediaDirection = function(mediaType) {
 
 	return 'sendrecv';
 };
-
-
